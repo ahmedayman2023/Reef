4af67160333c4580c2bc3aa3ec11bcd1d1edb223
Ahmed Ayman Abdelltif <122666938+ahmedayman2023@users.noreply.github.com>
2026-03-08 05:19:23 +0300

    image 1.1

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -91,7 +91,7 @@ const About = ({ isAr }: { isAr: boolean }) => {
           <div className="aspect-square rounded-2xl overflow-hidden shadow-2xl">
             <img 
               src="https://i.im.ge/2026/03/08/eghkF8.WhatsApp-Image-2026-02-25-at-14-05-22.jpeg" 
-              alt="Engineering Team" 
+              alt="" 
               className="w-full h-full object-cover"
               referrerPolicy="no-referrer"
             />
